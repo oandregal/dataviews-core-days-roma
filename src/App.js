18fd74c8583e415d0a78315e236441e3cafea944
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-06-26 16:19:27 +0200

    First example

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import { DataViews, filterSortAndPaginate } from '@wordpress/dataviews';
+import { useState } from 'react';
+
+import '@wordpress/dataviews/build-style/style.css';
+import '@wordpress/components/build-style/style.css';
+
+import { COUNCILS, PROVINCES } from './data';
+
+const formatNumber = (number) => new Intl.NumberFormat('en-EN').format(number);
+
+function App() {
+	const [data, setData] = useState(COUNCILS);
+
+	const [paginationInfo, setPaginationInfo] = useState({
+		totalItems: 0,
+		totalPages: 0,
+	});
+	const [view, setView] = useState({
+		type: 'table',
+		hiddenFields: ['img_src'],
+		perPage: 10,
+		layout: {
+			primaryField: 'name',
+			mediaField: 'img_src',
+			badgeFields: ['province'],
+			columnFields: [],
+		},
+		filters: [],
+	});
+
+	const fields = [
+		{
+			id: "img_src",
+			header: "Image",
+			render: ({ item }) => <a title={item.img_author} href={item.img_url}><img width="512" src={item.img_src} /></a>,
+			enableSorting: false,
+		},
+		{
+			id: 'name',
+			header: 'Name',
+			enableGlobalSearch: true,
+		},
+		{
+			id: 'province',
+			header: 'Province',
+			enableGlobalSearch: true,
+			elements: PROVINCES,
+		},
+		{
+			id: 'population',
+			header: 'Population',
+			render: ({ item }) => formatNumber(item.population),
+			enableSorting: false, // filterSortAndPaginate cannot sort by number types yet.
+		},
+		{
+			id: 'km_2',
+			header: 'Area (km²)',
+			render: ({ item }) => formatNumber(item.km_2),
+			enableSorting: false, // filterSortAndPaginate cannot sort by number types yet.
+		}
+	];
+
+	const onChangeView = (view) => {
+		const { data: newData, paginationInfo: newPaginationInfo } = filterSortAndPaginate(COUNCILS, view, fields);
+		setView(view);
+		setData(newData);
+		setPaginationInfo(newPaginationInfo);
+	}
+
+	return (
+		<DataViews
+			data={data}
+			fields={fields}
+			view={view}
+			onChangeView={onChangeView}
+			paginationInfo={paginationInfo}
+		/>
+	);
+}
+
+export default App;
